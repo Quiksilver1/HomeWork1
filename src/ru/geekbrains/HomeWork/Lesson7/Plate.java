ce2321ad30ddf4f50ee29b30cfe9b14a9ec5de47
Quiksilver1 <dzfa94@mail.ru>
2021-01-18 20:11:51 +0700

    Lesson7

--- src/ru/geekbrains/HomeWork/Lesson7/Plate.java
+++ src/ru/geekbrains/HomeWork/Lesson7/Plate.java
@@ -1,3 +1,37 @@
 package ru.geekbrains.HomeWork.Lesson7;
 
+public class Plate {
 
+    private int food;
+    private int additive;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public void info() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Plate{" +
+                "food=" + food +
+                '}';
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void additiveFood(int additive) {
+        System.out.println("Additive:" +additive);
+        this.food = food+additive;
+    }
+
+    public void decreaseFood(int appetite) {
+        if(this.food>=appetite){
+            this.food -= appetite;
+        }
+    }
+}
--- src/ru/geekbrains/HomeWork/Lesson7/String.java
+++ src/ru/geekbrains/HomeWork/Lesson7/String.java
@@ -1,3 +1,19 @@
 package ru.geekbrains.HomeWork.Lesson7;
 
 
+public class String {
+
+    public static void main(java.lang.String[] args) {
+        java.lang.String s="I like Java!!!";
+        System.out.println(s.charAt(s.length()-1));
+        System.out.println(s.endsWith("!!!"));
+        System.out.println(s.startsWith("I like"));
+        System.out.println(s.contains("Java"));
+        System.out.println(s.indexOf("Java"));
+        java.lang.String replace = s.replace('a', 'o');
+        System.out.println(replace);
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.substring(7,11));
+    }
+}
