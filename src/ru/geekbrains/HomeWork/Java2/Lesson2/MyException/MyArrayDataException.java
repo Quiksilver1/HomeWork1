b8b5d77f389a2984d721bff146790c94948a7377
Quiksilver1 <dzfa94@mail.ru>
2021-01-28 23:29:20 +0700

    add  exception

--- src/ru/geekbrains/HomeWork/Java2/Lesson2/MyException/MyArrayDataException.java
+++ src/ru/geekbrains/HomeWork/Java2/Lesson2/MyException/MyArrayDataException.java
@@ -1,3 +1,8 @@
 package ru.geekbrains.HomeWork.Java2.Lesson2.MyException;
 
 
+public class MyArrayDataException extends RuntimeException{
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
