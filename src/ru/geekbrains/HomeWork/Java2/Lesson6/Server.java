07dd386de96d73edd5bcf8d67836c6895d310b6e
Quiksilver1 <dzfa94@mail.ru>
2021-02-15 19:21:12 +0700

    Lesson6+

--- src/ru/geekbrains/HomeWork/Java2/Lesson6/Server.java
+++ src/ru/geekbrains/HomeWork/Java2/Lesson6/Server.java
@@ -7,36 +7,46 @@ import java.net.Socket;
 
 public class Server extends JFrame {
 
+
     public static void main(String[] args) throws InterruptedException {
         Socket socket;
         try (ServerSocket serverSocket = new ServerSocket(8082)) {
-            BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Server start");
             socket = serverSocket.accept();
             System.out.println("Client join");
             DataInputStream dis = new DataInputStream(socket.getInputStream());
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            while (true) {
-
-
-                System.out.print("Client wrote you: ");
-                String message = dis.readUTF();
-                System.out.println(message);
-                if (message.equalsIgnoreCase("/finish")) {
-                    dos.writeUTF(message);
-                    break;
+            Thread out=new Thread(()-> {
+                try {
+                    while (true) {
+                        System.out.println("Write your message: ");
+                        String serverCommand = br.readLine();
+                        dos.writeUTF(serverCommand);
+                    }
+                }catch (IOException ignored){
                 }
-                Thread.sleep(1000);
-                System.out.print("Write your message: ");
-                String serverCommand = br.readLine();
-                dos.writeUTF(serverCommand);
-                Thread.sleep(1000);
+            });
+            Thread in=new Thread(()-> {
+                try {
+                    while (true) {
+                        String message = dis.readUTF();
+                        System.out.println("Client wrote you: "+message);
 
+                        //System.out.println(message);
+                        if (message.equalsIgnoreCase("/finish")) {
+                            dos.writeUTF(message);
+                            break;
+                        }
+                    }
+                }catch (IOException ignored){
+                }
+            });
 
-            }
+            out.start();
+            in.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
